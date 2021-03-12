640e22f227c0bac2eb90f8683c19c764e99de9cb
savinov <savinov_vu@mail.ru>
2021-03-12 23:05:28 +0800

    создан UtilService (бин объявлен при помощи xml); бин UtilService внедрен в Бин xmlservice (с помощью xml конфигурации)

--- src/main/java/ru/savinov/springbeans/service/HelloXmlService.java
+++ src/main/java/ru/savinov/springbeans/service/HelloXmlService.java
@@ -2,9 +2,17 @@ package ru.savinov.springbeans.service;
 
 public class HelloXmlService {
 
-    public String getHelloXml() {
-        return "Hello xml";
+    private UtilService utilService;
+
+    public UtilService getUtilService() {
+        return utilService;
     }
 
+    public void setUtilService(UtilService utilService) {
+        this.utilService = utilService;
+    }
 
+    public String getHelloXml() {
+        return utilService.append("Hello from xml");
+    }
 }
---
+++ src/main/java/ru/savinov/springbeans/service/UtilService.java
@@ -0,0 +1,8 @@
+package ru.savinov.springbeans.service;
+
+public class UtilService {
+
+    public String append(String message) {
+        return message + "  "+ message;
+    }
+}
